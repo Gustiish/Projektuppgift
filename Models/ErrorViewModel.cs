b69cd545c07104f01710ea2a1349999e5af48eb9
Gustiish <isak.backstrom@gmail.com>
2025-05-12 17:07:54 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Projektuppgift.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
