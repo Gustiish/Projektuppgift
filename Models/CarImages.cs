353d233c9324324675bc0e0f2f6fb1dd20c0efde
Gustiish <isak.backstrom@gmail.com>
2025-05-29 11:10:31 +0200

    Fixed images js

---
+++ Models/CarImages.cs
@@ -0,0 +1,10 @@
+﻿namespace Projektuppgift.Models
+{
+    public class CarImages
+    {
+        public int Id { get; set; }
+        public string stringURl { get; set; }
+        public int CarRentalId  { get; set; }
+        public CarRental CarRental { get; set; }
+    }
+}
--- ViewModels/CarEditViewModel.cs
+++ ViewModels/CarEditViewModel.cs
@@ -7,7 +7,7 @@ namespace Projektuppgift.ViewModels
         public int Id { get; set; }
         public string Brand { get; set; }
         public string Model { get; set; }
-        public List<string> Images { get; set; }
+        public List<string>? Images { get; set; }
         public bool IsBooked { get; set; } = false;
         public CustomerOrder? customerOrder { get; set; }
         public int? CustomerOrderId { get; set; }
